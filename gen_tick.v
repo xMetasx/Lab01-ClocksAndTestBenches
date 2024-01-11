cafc278826f84fe7e5d5455056916a3d730c4381
Allan Knight <67027104+ucrdrk@users.noreply.github.com>
2024-01-11 13:01:45 -0800

    Update gen_tick.v
    Added comment header

--- gen_tick.v
+++ gen_tick.v
@@ -1,3 +1,18 @@
+//=========================================================================
+// Name & Email must be EXACTLY as in Gradescope roster!
+// Name: 
+// Email: 
+// 
+// Assignment name: 
+// Lab section: 
+// TA: 
+// 
+// I hereby certify that I have not received assistance on this assignment,
+// or used code, from ANY outside source other than the instruction team
+// (apart from what was provided in the starter file).
+//
+//=========================================================================
+
 module gen_tick # ( parameter SRC_FREQ = 5000, parameter TICK_FREQ = 1) (
     input src_clk,
     input enable,
